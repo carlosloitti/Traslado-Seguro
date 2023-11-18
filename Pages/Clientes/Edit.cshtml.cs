31e8ae6e612fe61e898a7f9b9f24f1d8b1c62800
Carlos Loitti <carlosloitir@gmail.com>
2023-11-18 17:27:26 -0500

    Edit Clientes

---
+++ Pages/Clientes/Edit.cshtml.cs
@@ -0,0 +1,79 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Traslado_Seguro.Data;
+using Traslado_Seguro.Model;
+
+namespace Traslado_Seguro.Pages.Clientes
+{
+    public class EditModel : PageModel
+    {
+        private readonly TrasladoSeguroContext _context;
+
+        public EditModel(TrasladoSeguroContext context)
+        {
+            _context = context;
+        }
+
+
+        [BindProperty]
+
+        public Cliente Cliente { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Clientes == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            Cliente = cliente;
+            return Page();
+
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Cliente).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClienteExists(Cliente.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ClienteExists(int id)
+        {
+
+            return (_context.Clientes?.Any(e => e.Id == id)).GetValueOrDefault();
+
+        }
+
+    }
+}
