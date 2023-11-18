026bdeeffe3e564ff2608fd67e0bb0787abf273e
Carlos Loitti <carlosloitir@gmail.com>
2023-11-18 15:15:36 -0500

    Model User - Razor Page Login

---
+++ Pages/Account/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Traslado_Seguro.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
