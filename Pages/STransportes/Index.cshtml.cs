8c63f54e56e93976cdf0b2e06db470d0173bd657
Carlos Loitti <carlosloitir@gmail.com>
2023-11-18 17:35:09 -0500

    Index Servicio Transporte

---
+++ Pages/STransportes/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Traslado_Seguro.Data;
+using Traslado_Seguro.Model;
+
+namespace Traslado_Seguro.Pages.STransportes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly TrasladoSeguroContext _context;
+
+        public IndexModel(TrasladoSeguroContext context)
+        {
+            _context = context;
+        }
+
+        public IList<STransporte> Transportes { get; set; } = default!;
+
+
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Transportes != null)
+            {
+                Transportes = await _context.Transportes.ToListAsync();
+            }
+
+        }
+    }
+}
